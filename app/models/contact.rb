f7ef19b7d0265a28ab8ca7323b26bd7b0c0f0553
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-23 12:43:03 -0400

    Add contact model manually

---
+++ app/models/contact.rb
@@ -0,0 +1,12 @@
+class Contact
+    attr_accessor :first_name, :last_name, :email, :phone_number, :favorites
+
+    def initialize(options)
+        @first_name = options[:first_name]
+        @last_name = options[:last_name]
+        @email = options[:email]
+        @phone_number = options[:phone_number]
+        @favorites = options[:favorites]
+    end
+
+end
