b388cff47c06e337edbeac944991db4f69f071ba
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-24 10:20:48 -0400

    Routes and controller for all contacts - long way

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+
+    #for one contact
     def one_contact
         contact = Contact.first
         render json: {
@@ -9,4 +11,32 @@ class ContactsController < ApplicationController
             phone_number: contact[:phone_number]
         }
     end
+
+    #for all contacts
+    def all_contacts
+        contacts = Contact.all
+        render json: [{
+            id: contacts[0][:id],
+            first_name: contacts[0][:first_name],
+            last_name: contacts[0][:last_name],
+            email: contacts[0][:email],
+            phone_number: contacts[0][:phone_number]
+        },
+        {
+            id: contacts[1][:id],
+            first_name: contacts[1][:first_name],
+            last_name: contacts[1][:last_name],
+            email: contacts[1][:email],
+            phone_number: contacts[1][:phone_number]
+        },
+        {
+            id: contacts[2][:id],
+            first_name: contacts[2][:first_name],
+            last_name: contacts[2][:last_name],
+            email: contacts[2][:email],
+            phone_number: contacts[2][:phone_number]
+        }
+    ]
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # for one contact
   get "/one_contact", controller: "contacts", action: "one_contact"
+
+  #for all contacts
+  get "/all_contacts" => "contacts#all_contacts"
+
 end
