ea702d22d1f58f141c3b83f695f81f9320b15940
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-24 10:36:33 -0400

    Add template for all contacts method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,30 +8,10 @@ class ContactsController < ApplicationController
     end
 
     #for all contacts
+    # .json needed in url
     def all_contacts
-        contacts = Contact.all
-        render json: [{
-            id: contacts[0][:id],
-            first_name: contacts[0][:first_name],
-            last_name: contacts[0][:last_name],
-            email: contacts[0][:email],
-            phone_number: contacts[0][:phone_number]
-        },
-        {
-            id: contacts[1][:id],
-            first_name: contacts[1][:first_name],
-            last_name: contacts[1][:last_name],
-            email: contacts[1][:email],
-            phone_number: contacts[1][:phone_number]
-        },
-        {
-            id: contacts[2][:id],
-            first_name: contacts[2][:first_name],
-            last_name: contacts[2][:last_name],
-            email: contacts[2][:email],
-            phone_number: contacts[2][:phone_number]
-        }
-    ]
+        @contacts = Contact.all
+        render :index
     end
 
 end
