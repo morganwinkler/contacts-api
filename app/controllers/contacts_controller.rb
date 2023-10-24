5a3382ebbe1289c23884f022a09ae930a3569f27
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-24 10:32:36 -0400

    Add single contact template to method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,10 @@
 class ContactsController < ApplicationController
 
     #for one contact
+    # dont forget to put .json in url
     def one_contact
-        contact = Contact.first
-        render json: {
-            id: contact[:id],
-            first_name: contact[:first_name],
-            last_name: contact[:last_name],
-            email: contact[:email],
-            phone_number: contact[:phone_number]
-        }
+        @contact = Contact.first
+        render :show
     end
 
     #for all contacts
